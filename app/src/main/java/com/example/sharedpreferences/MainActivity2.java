6c96bb9cb90868c0847876a92d86dced4f315c11
chucoyos <facturasmanzanillo@hotmail.com>
2021-02-24 16:08:06 -0600

    add second activity

--- app/src/main/java/com/example/sharedpreferences/MainActivity2.java
+++ app/src/main/java/com/example/sharedpreferences/MainActivity2.java
@@ -10,37 +10,35 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity2 extends AppCompatActivity {
 
-    EditText editText, editText1;
+    EditText editText3, editText4;
     Button button;
     SharedPreferences preferences;
-    private static final String data = "data";
     private static final String name = "name";
     private static final String lastName = "lastName";
+    private static final String data = "data";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-
-        editText = findViewById(R.id.editTextTextPersonName);
-        editText1 = findViewById(R.id.editTextTextPersonName2);
-        button = findViewById(R.id.button);
+        editText3 = findViewById(R.id.editTextTextPersonName3);
+        editText4 = findViewById(R.id.editTextTextPersonName4);
+        button = findViewById(R.id.button2);
 
         preferences = getSharedPreferences(data, Context.MODE_PRIVATE);
-        editText.setText(preferences.getString(name, "" ));
-        editText1.setText(preferences.getString(lastName, ""));
+        editText3.setText(preferences.getString(name, ""));
+        editText4.setText(preferences.getString(lastName, ""));
     }
 
-    public void addPref(View view){
-        preferences = getSharedPreferences(data, Context.MODE_PRIVATE);
+    public void setData(View view){
         SharedPreferences.Editor editor = preferences.edit();
-        editor.putString(name, editText.getText().toString());
-        editor.putString(lastName, editText1.getText().toString());
+        editor.putString(name, editText3.getText().toString());
+        editor.putString(lastName, editText4.getText().toString());
         editor.apply();
-        Intent intent = new Intent(this, MainActivity2.class);
+        Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
 }
